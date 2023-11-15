28090121d52e9f04c80db78073060060d0e86c81
Kaltum-AVG <kaltum.viveros@alumno.buap.mx>
2023-11-14 18:53:11 -0600

    screens de registro, juego-terminado, home y login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,53 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginScreenComponent } from './screens/login-screen/login-screen.component';
 
+//Angular material
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+//Componentes
+import { RegistroScreenComponent } from './screens/registro-screen/registro-screen.component';
+import { HomeScreenComponent } from './screens/home-screen/home-screen.component';
+import { HeaderAppComponent } from './partials/header-app/header-app.component';
+import { LeftSidebarComponent } from './partials/left-sidebar/left-sidebar.component';
+import { JuegoTerminadoScreenComponent } from './screens/juego-terminado-screen/juego-terminado-screen.component';
+import { FooterAppComponent } from './partials/footer-app/footer-app.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginScreenComponent,
+    RegistroScreenComponent,
+    HomeScreenComponent,
+    HeaderAppComponent,
+    LeftSidebarComponent,
+    JuegoTerminadoScreenComponent,
+    FooterAppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDatepickerModule,
+    FormsModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatProgressSpinnerModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
